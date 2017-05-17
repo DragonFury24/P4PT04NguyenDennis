dcd1450f2786ce8987e97adefc07e873d42ac9ce
DragonFury24 <dennisdnguyen19@gmail.com>
2017-05-17 11:13:16 -0400

    .

--- app/src/main/java/com/dragonfury/duy/p4pt04nguyendennis/DrawView.java
+++ app/src/main/java/com/dragonfury/duy/p4pt04nguyendennis/DrawView.java
@@ -2,6 +2,8 @@ package com.dragonfury.duy.p4pt04nguyendennis;
 
 import android.graphics.Canvas;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,21 +15,43 @@ import android.content.Context;
 public class DrawView extends View {
     public DrawView(Context context) {
         super(context);
-        context.getSystemService(Context.SENSOR_SERVICE);
+
+        sensorManager = (sensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+
+        listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float[] accelerometer = sensorEvent.values;
+                if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+                    accelerometer[0] = sensorEvent.values[0];
+                    accelerometer[1] = sensorEvent.values[1];
+                    accelerometer[2] = sensorEvent.values[2];
+                    degrees = (float) Math.toDegrees(Math.atan(Math.abs(accelerometer[1]) / Math.abs(accelerometer[2])));
+
+
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
     }
 
-    SensorManager sensorManager = new SensorManager();
-    float[] orientation = new float[3];
-    float[] accelerometer = new float[3];
-    SensorActivity sensorActivity = new SensorActivity();
+    private float[] orientation = new float[3];
+    private float[] accelerometer = new float[3];
+    private Sensor sensor;
+    private SensorManager sensorManager;
+    private SensorEventListener listener;
+    private MainActivity mainActivity;
+    private float degrees;
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        getContext().getSystemService(Context.SENSOR_SERVICE);
-        accelerometer = Sensor.TYPE_ACCELEROMETER;
-        orientation = sensorActivity.getOrientation(sensorManager.getRotationMatrix(null, null, Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_MAGNETIC_FIELD), orientation);
+
     }
 
 }
