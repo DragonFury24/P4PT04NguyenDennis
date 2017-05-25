48d9c188c70f5256b324f0c621f38d269862f431
Duy <dennisdnguyen19@gmail.com>
2017-05-24 23:28:58 -0400

    .

--- app/src/main/java/com/dragonfury/duy/p4pt04nguyendennis/SensorActivity.java
+++ app/src/main/java/com/dragonfury/duy/p4pt04nguyendennis/SensorActivity.java
@@ -49,6 +49,9 @@ public class SensorActivity extends Activity implements SensorEventListener{
 
         sensorManager.getRotationMatrix(rotateMatrix, null, accelerometer, geoField); //Calculate RotationMatrix - Device screen is facing sky, top is towards north pole
         sensorManager.getOrientation(rotateMatrix, orientations); //Calculate amount of rotation along x, y, and z axis
+        for (float orientation: orientations) {
+            System.out.println("test:" + orientation);
+        }
         //System
     }
 
